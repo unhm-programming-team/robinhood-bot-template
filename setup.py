931afccd123a52780caf683ee8255971c950ebf3
klm127 <karlmiller127@gmail.com>
2021-11-19 13:42:26 -0500

    added requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='robinhood-bot-template',
+    version='0.0.1',
+    packages=[],
+    url='',
+    license='',
+    author='unhm-programming-team',
+    author_email='',
+    description='a bot for trading stocks'
+)
